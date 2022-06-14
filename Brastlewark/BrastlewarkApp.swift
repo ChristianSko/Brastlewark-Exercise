b10c9045ec4e2311cba1640ed64da92fce369e48
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-06-14 15:56:26 +0200

    Initial Commit

---
+++ Brastlewark/BrastlewarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrastlewarkApp.swift
+//  Brastlewark
+//
+//  Created by Skorobogatow, Christian on 14/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BrastlewarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
